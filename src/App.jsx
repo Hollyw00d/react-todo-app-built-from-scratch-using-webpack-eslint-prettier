6f01b339f1455fe9c34bce33c011afea7f12f389
Matt Jennings <matt@mattjennings.net>
2023-07-10 18:17:27 -0700

    learned how to pass value from child to parent

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { TodoList } from "./components/TodoList/TodoList.jsx";
 import { TodoInputField } from "./components/TodoInputField/TodoInputField.jsx";
 
 const App = () => {
+  const onInputField = (newTodo) => {
+    console.log(newTodo);
+  };
+
   return (
     <div>
       <h1>Todo App</h1>
-      <TodoInputField />
+      <TodoInputField onSubmit={onInputField} />
       <TodoList />
     </div>
   );
--- src/components/TodoInputField/TodoInputField.jsx
+++ src/components/TodoInputField/TodoInputField.jsx
@@ -1,10 +1,31 @@
-const TodoInputField = () => {
+import { useState } from "react";
+
+const TodoInputField = (props) => {
+  const { onSubmit } = props;
+
+  const [newTodo, setNewTodo] = useState("");
+
+  const onInputChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const onInputFieldSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(newTodo);
+  };
+
   return (
-    <form>
+    <form onSubmit={onInputFieldSubmit}>
       <h2>
         <label htmlFor="new-todo">What needs to be done</label>
       </h2>
-      <input id="new-todo" type="text" />
+      <input
+        id="new-todo"
+        type="text"
+        value={newTodo}
+        onChange={onInputChange}
+      />
+      <button type="submit">Add Todo</button>
     </form>
   );
 };
