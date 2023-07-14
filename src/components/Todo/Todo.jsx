eb13fb50e0cf499bc605b0bab7cbb4ca38cac377
Matt Jennings <matt@mattjennings.net>
2023-07-14 16:33:28 -0700

    added correct boolean

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,17 +1,40 @@
+import { Undo, Checkmark, Edit, Trash } from "grommet-icons";
+import "./todo.css";
+
 const Todo = ({ name, completed, id, toggleCompletion }) => {
   const doneClass = "done";
   const notDoneClass = "not-done";
 
   return (
-    <li key={id} style={{ textDecoration: completed ? "line-through" : null }}>
-      {name}
-      <button
-        onClick={toggleCompletion}
-        className={completed ? doneClass : notDoneClass}
-        data-item-name={name}
-      >
-        {completed ? "Mark Not Done" : "Mark Done"}
-      </button>
+    <li key={id}>
+      <div>
+        <span style={{ textDecoration: completed ? "line-through" : null }}>
+          {name}
+        </span>
+      </div>
+      <div className="btns-container">
+        <button
+          onClick={toggleCompletion}
+          className={completed ? doneClass : notDoneClass}
+          data-item-name={name}
+        >
+          {completed ? (
+            <>
+              Mark Not Done <Undo color="orange" />
+            </>
+          ) : (
+            <>
+              Mark Done <Checkmark color="green" />
+            </>
+          )}
+        </button>
+        <button>
+          Edit <Edit color="blue" />
+        </button>
+        <button>
+          Delete <Trash color="red" />
+        </button>
+      </div>
     </li>
   );
 };
